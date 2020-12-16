ce0fd1a517ac18a5b8581733939ec25677aae6fe
Your Name <you@example.com>
2020-12-16 21:11:38 +0800

    quartz

---
+++ NetCore.Quartz/DemoJob.cs
@@ -0,0 +1,26 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore.Quartz
+{
+    public class DemoJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.Run(() =>
+            {
+                var wtime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                string path = "/demo.log";
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine("执行作业" + wtime);
+                }
+            });
+        }
+    }
+}
---
+++ NetCore.Quartz/Program.cs
@@ -0,0 +1,26 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading;
+
+namespace NetCore.Quartz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IScheduler scheduler;
+            ISchedulerFactory factory = new StdSchedulerFactory();
+            scheduler = factory.GetScheduler().Result;
+            IJobDetail testJobDetail = JobBuilder.Create<DemoJob>().WithIdentity("DemoJob").Build();
+            ITrigger testJobTrigger = TriggerBuilder.Create().WithCronSchedule("0/5 * * * * ? *").Build();
+            scheduler.ScheduleJob(testJobDetail, testJobTrigger);
+            scheduler.Start();
+            while (true)
+            { 
+            }
+        }
+    }
+}
